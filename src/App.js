7cb59ecc56ebbce8b83a54b8c991d083688ec968
sushma1031 <sushmajram@gmail.com>
2024-10-21 11:31:30 +0530

    update homepage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import { Navigate, Route, Routes} from "react-router-dom";
 
 import Navbar from "./components/navbar/Navbar";
 import Socials from "./components/navbar/Socials";
+// import HomePage from "./components/pages/HomePage";
 import HomePage from "./components/pages/HomePage";
-import EventsPage from "./components/pages/EventsPage";
 import RegisterPage from "./components/pages/RegisterPage";
 import TeamPage from "./components/pages/TeamPage";
 import AboutPage from "./components/pages/AboutPage";
-import EventPageLink from "./components/buttons/EventPageLink";
+// import EventPageLink from "./components/buttons/EventPageLink";
 
 function App() {
   return (
@@ -15,14 +15,13 @@ function App() {
       <Navbar />
       <Routes>
         {/* events page is landing page here */}
-        <Route path="/" element={<EventsPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/event" element={<Navigate to={{ pathname: "/" }} />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<Navigate to={{ pathname: "/" }} />} />
+        {/* <Route path="/event" element={<Navigate to={{ pathname: "/" }} />} /> */}
         <Route path="/team" element={<TeamPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
-      <EventPageLink/>
       <Socials />
     </div>
   );
