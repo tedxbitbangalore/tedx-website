25324d97d997431e960e69b72bd8ae7bb915be0a
sushma1031 <sushmajram@gmail.com>
2024-11-11 11:49:07 +0530

    update core team pics

--- src/components/pages/TeamPage.jsx
+++ src/components/pages/TeamPage.jsx
@@ -6,7 +6,13 @@ import MemberCardTwo from "../cards/MemberCardTwo";
 
 //Images Resources
 import imgPrincipal from "../../resources/images/team/principal.webp";
-import imgContent from "../../resources/images/team/Nikita_Tenjerla@Head_Of_Content.webp";
+import imgContent from "../../resources/images/team/Sushma.png";
+import imgCreative from "../../resources/images/team/Ganga.jpg";
+import imgTreasury from "../../resources/images/team/Shwet.webp";
+import imgCoOrg2 from "../../resources/images/team/Khushi.jpg";
+import imgMarketing from "../../resources/images/team/Apeksha.jpg";
+import imgCuration from "../../resources/images/team/Sourabh.jpg";
+import imgCoOrg1 from "../../resources/images/team/Nithyashree.jpg";
 
 const TeamPage = () => {
   return (
@@ -20,7 +26,7 @@ const TeamPage = () => {
       <MemberCardTwo
         names={["Nithyashree M.", "Khushi V."]}
         titles={["Co-Organiser", "Co-Organiser"]}
-        paths={[imgContent, imgContent]}
+        paths={[imgCoOrg1, imgCoOrg2]}
         description={`The creative architects behind our transformative gatherings. The driving forcea that are responsible for capturing the spirit of TED in the event by sparking community connections and inspiring ideas that change perspectives. They are the guiding hands, uniting impassioned individuals to create a powerful, unforgettable event. In essence, they are the catalyst for innovation and positive change.`}
       />
       <MemberCard
@@ -32,26 +38,26 @@ const TeamPage = () => {
       <MemberCard
         name="Sourabh P."
         title="Head of Curation"
-        path={imgContent}
+        path={imgCuration}
         description={`The visionary force behind our extraordinary events. Their mission? To seek, select, and cultivate the most compelling ideas, bringing them to our stage to inspire and transform the world, headed by a resilient and visionary head who holds pivotal role in shaping the essence and impact of our event. These dedicated individuals are the gatekeepers of innovation, tirelessly working to curate a line-up that reflects the very essence of "Ideas Worth Spreading."`}
       />
       <MemberCard
         name="Ganga G."
         title="Head of Creative"
-        path={imgContent}
+        path={imgCreative}
         description={`The Creative Team comprises the idea architects, problem solvers, and pioneers who stretch the boundaries of what can be achieved. With the skill of painters, they create intricate brushstrokes on a blank canvas. 
           Dedicated to their craft, they invest tireless effort in idea exploration, concept development, prototyping, refinement, and the invaluable feedback loop. It is their magic that gives each event its distinct pulse, weaving together the threads of imagination to create moments that linger in the memory, inspiring and illuminating.`}
       />
       <MemberCard
         name="Apeksha B. A."
         title="Head of Marketing"
-        path={imgContent}
+        path={imgMarketing}
         description={`The Marketing Team plays a pivotal role in expanding the reach of our creations to a wider audience. Under the leadership of the head , they function as an attractive force, much like a beautiful flower that draws in a friendly crowd with its appealing nectar. Through their carefully crafted campaigns, they skillfully orchestrate engagement, directing our audience to the most compelling aspects of our event. Acting as a persuasive link between our visions and the outside world, they infuse our creations with energy and enthusiasm, making them engaging and impactful.`}
       />
       <MemberCard
         name="Shwet K."
         title="Head of Treasury"
-        path={imgContent}
+        path={imgTreasury}
         description={`The financial backbone of TEDxBITBangalore, the Treasury is responsible for managing the organisation's financial resources. Balancing budgets, making informed financial decisions and providing strategic financial oversight, the Treasury enables TEDxBITBangalore to deliver impactful experiences while maintaining fiscal responsibility and long-term sustainabiity.`}
       />
     </div>
